124ec90f6efb52d4e3a73b28d80eed7056a6131b
Zsolt Revay <zsolt.revay.g@gmail.com>
2023-02-04 19:09:18 +0100

    Ignore lint fontpacks

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	extends: ['@dekode/stylelint-config'],
-	ignoreFiles: ['packages/themes/wps-prime/assets/font-packs/**/*.css'],
+	ignoreFiles: ['assets/font/**/*.css'],
 	rules: {
 		'at-rule-no-unknown': [
 			true,
