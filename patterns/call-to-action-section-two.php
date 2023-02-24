ce130f658192367ecce22eaf0abc17b3ad2fac9c
Zsolt Revay <zsolt.revay.g@gmail.com>
2023-02-24 21:43:38 +0100

    Update gravity style / Add cta two

---
+++ patterns/call-to-action-section-two.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Call to Action Section Two
+ * Slug: xeno/call-to-action-section-two
+ * Categories: xeno-call-to-action
+ * Block Types: xeno/template-part/call-to-action
+ *
+ * @package Xeno
+ */
+
+declare( strict_types=1 );
+?>
+<!-- wp:wps/section {"dimRatio":50,"align":"","spacingVertical":"","backgroundColor":"light","focalPoint":{},"media":{},"backgroundBehaviour":"contain","marginBottom":"","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","right":"var:preset|spacing|normal","bottom":"var:preset|spacing|large","left":"var:preset|spacing|normal"}}}} -->
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Continually engineer market positioning technologies.</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}}} -->
+<p class="has-link-color">Credibly simplify mission-critical markets via resource-leveling scenarios. Globally plagiarize one-to-one customer service for fully tested information. Intrinsicly empower <a href="#home">cross-platform sources</a> through bleeding-edge vortals. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p class=""><br>Distinctively productivate value-added core competencies for transparent "outside the box" thinking. Intrinsicly network team driven imperatives rather than low-risk high-yield networks.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+	<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Click here</a></div>
+	<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+<!-- /wp:wps/section -->
