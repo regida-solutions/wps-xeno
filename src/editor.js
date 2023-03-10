5a27d9559a0272f66d44c8179b99ded21e96aa96
Zsolt Revay <zsolt.revay.g@gmail.com>
2023-03-10 15:05:16 +0100

    Add cover blocks styles

--- src/editor.js
+++ src/editor.js
@@ -20,5 +20,47 @@ window.wp.domReady(() => {
 			label: 'Highlighted',
 			isDefault: false,
 		});
+
+		// decorations
+		window.wp.blocks.registerBlockStyle('core/cover', {
+			name: 'angled-up',
+			label: 'Angled up',
+			isDefault: false,
+		});
+
+		window.wp.blocks.registerBlockStyle('core/cover', {
+			name: 'angled-down',
+			label: 'Angled down',
+			isDefault: false,
+		});
+
+		window.wp.blocks.registerBlockStyle('core/cover', {
+			name: 'arrow-up',
+			label: 'Arrow up',
+			isDefault: false,
+		});
+
+		window.wp.blocks.registerBlockStyle('core/cover', {
+			name: 'arrow-down',
+			label: 'Arrow down',
+			isDefault: false,
+		});
+
+		window.wp.blocks.registerBlockStyle('core/cover', {
+			name: 'circle',
+			label: 'Circle',
+			isDefault: false,
+		});
+
+		window.wp.blocks.registerBlockStyle('core/cover', {
+			name: 'rounded',
+			label: 'Rounded',
+			isDefault: false,
+		});
+		window.wp.blocks.registerBlockStyle('core/cover', {
+			name: 'wave',
+			label: 'Wave',
+			isDefault: false,
+		});
 	});
 });
