99694c543895b6bae4056b5119dad6632c946f07
Zsolt Revay <zsolt.revay.g@gmail.com>
2023-05-26 07:23:21 +0200

    Add helper classes

--- src/editor.js
+++ src/editor.js
@@ -1,66 +1,76 @@
 /**
- * Internal dependencies
+ * WordPress dependencies
  */
-window.wp.domReady(() => {
+import domReady from '@wordpress/dom-ready';
+import { registerBlockStyle } from '@wordpress/blocks';
+
+domReady(() => {
 	// column/card item settings
 	['wps/grid-column', 'wps/card'].forEach((element) => {
-		window.wp.blocks.registerBlockStyle(element, {
+		registerBlockStyle(element, {
 			name: 'shadow-hover',
 			label: 'Shadow hover',
 			isDefault: false,
 		});
 
-		window.wp.blocks.registerBlockStyle(element, {
+		registerBlockStyle(element, {
 			name: 'bordered',
 			label: 'Bordered',
 			isDefault: false,
 		});
-		window.wp.blocks.registerBlockStyle(element, {
+		registerBlockStyle(element, {
 			name: 'highlighted',
 			label: 'Highlighted',
 			isDefault: false,
 		});
 
 		// decorations
-		window.wp.blocks.registerBlockStyle('core/cover', {
+		registerBlockStyle('core/cover', {
 			name: 'angled-up',
 			label: 'Angled up',
 			isDefault: false,
 		});
 
-		window.wp.blocks.registerBlockStyle('core/cover', {
+		registerBlockStyle('core/cover', {
 			name: 'angled-down',
 			label: 'Angled down',
 			isDefault: false,
 		});
 
-		window.wp.blocks.registerBlockStyle('core/cover', {
+		registerBlockStyle('core/cover', {
 			name: 'arrow-up',
 			label: 'Arrow up',
 			isDefault: false,
 		});
 
-		window.wp.blocks.registerBlockStyle('core/cover', {
+		registerBlockStyle('core/cover', {
 			name: 'arrow-down',
 			label: 'Arrow down',
 			isDefault: false,
 		});
 
-		window.wp.blocks.registerBlockStyle('core/cover', {
+		registerBlockStyle('core/cover', {
 			name: 'circle',
 			label: 'Circle',
 			isDefault: false,
 		});
 
-		window.wp.blocks.registerBlockStyle('core/cover', {
+		registerBlockStyle('core/cover', {
 			name: 'rounded',
 			label: 'Rounded',
 			isDefault: false,
 		});
-		window.wp.blocks.registerBlockStyle('core/cover', {
+		registerBlockStyle('core/cover', {
 			name: 'wave',
 			label: 'Wave',
 			isDefault: false,
 		});
+	});
+
+	/* Gravity styles */
+	registerBlockStyle('gravityforms/form', {
+		name: 'xeno-style',
+		label: 'Custom style',
+		isDefault: false,
 	});
 });
